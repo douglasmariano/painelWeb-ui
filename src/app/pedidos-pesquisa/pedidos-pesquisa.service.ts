7348940eb30c4551fd9f29fd7f11c5236ee9d3a4
Douglas <douglasmariano.com@gmail.com>
2021-01-14 23:30:30 -0300

    Erro no form do editar cadastro, ngModel esta errado

--- src/app/pedidos-pesquisa/pedidos-pesquisa.service.ts
+++ src/app/pedidos-pesquisa/pedidos-pesquisa.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pedido } from './model';
 
@@ -16,10 +16,11 @@ export class PedidoPesquisaService {
   pesquisar(filtro: any): Promise<any> {
     //const params = new URLSearchParams();
     // return this.http.get(`${this.pedidosUrl}?dataPedidoDe=2020-10-02&dataPedidoAte=2020-10-03`)
-
+    const headers = new HttpHeaders()
+    .append('Content-Type', 'application/json');
     if (filtro) {
-      // params.set('numped', filtro.numped);   
-      return this.http.post(this.pedidosUrl, filtro)
+      // params.set('numped', filtro.numped);
+      return this.http.post(this.pedidosUrl, headers,filtro)
         .toPromise();
     }
   }
@@ -31,6 +32,8 @@ export class PedidoPesquisaService {
   }
 
   adicionar(pedido: Pedido): Promise<Pedido> {
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json');
     return this.http.post<Pedido>(this.pedidosUrlAll, pedido, {  })
     .toPromise();
   }
